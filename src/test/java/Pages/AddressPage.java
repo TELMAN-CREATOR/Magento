133111ae58a4a6cd0a7d072dbbe7aa95671fbbf7
Mert-Gocmen <mert33gocmen@gmail.com>
2025-05-21 03:23:02 +0300

    US703 BİTTİ

---
+++ src/test/java/Pages/AddressPage.java
@@ -0,0 +1,46 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.junit.Assert.assertTrue;
+
+public class AddressPage {
+    WebDriver driver;
+
+    public AddressPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickAddNewAddress() {
+        driver.findElement(By.linkText("Add New Address")).click();
+    }
+
+    public void fillAddressForm(String firstname, String lastname, String street, String city, String postcode, String country, String phone) {
+        driver.findElement(By.id("firstname")).clear();
+        driver.findElement(By.id("firstname")).sendKeys(firstname);
+
+        driver.findElement(By.id("lastname")).clear();
+        driver.findElement(By.id("lastname")).sendKeys(lastname);
+
+        driver.findElement(By.name("street[0]")).sendKeys(street);
+        driver.findElement(By.id("city")).sendKeys(city);
+
+        Select countryDropdown = new Select(driver.findElement(By.id("country")));
+        countryDropdown.selectByVisibleText(country);
+
+        driver.findElement(By.id("zip")).sendKeys(postcode);
+        driver.findElement(By.id("telephone")).sendKeys(phone);
+    }
+
+    public void saveAddress() {
+        driver.findElement(By.cssSelector("button.save")).click();
+    }
+
+    public void verifySuccessMessage() {
+        assertTrue(driver.getPageSource().contains("You saved the address."));
+    }
+}
+
+
